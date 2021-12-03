12b897f7b05e2110b558620eebd0bd0cd98d429d
Клинин Виктор Андреевич <Viktor.Klinin@evraz.com>
2021-12-03 08:57:19 +0500

    hw1 done

---
+++ HW1 - install/version.sql
@@ -0,0 +1,9 @@
+/*
+1. Создайте github-репозиторий для хранения выполненных домашних заданий.
+2. Установите SQL Server 2016\2017\2019 и SQL Server Management Studio.
+3. Разверните у себя бэкап базы WideWorldImporters (из файла WideWorldImporters-Full.bak).
+Сделайте скриншот с восстановленной БД WideWorldImporters и выполненным select @@version.
+Скриншот разместите в вашем github-репозитории.
+4. Прочитайте описание БД WideWorldImporters.
+*/
+select @@version
